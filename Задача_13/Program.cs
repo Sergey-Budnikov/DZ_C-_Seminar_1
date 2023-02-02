844ce1f583318cec203d0afd1323028c6fa095ac
Будников Сергей Юрьевич <mr.s.b@bk.ru>
2023-02-03 02:05:15 +0300

    Решение Задачи 13

--- Задача_13/Program.cs
+++ Задача_13/Program.cs
@@ -3,4 +3,25 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int Value;
+if (!int.TryParse(Console.ReadLine(), out Value))
+{
+    Console.WriteLine("Вы ввели не числовое значение!");
+    return;
+}
+
+int ValueAbs = Math.Abs(Value);   // Это для того, чтобы проверять отрицательные
+
+if (ValueAbs < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+    return;
+}
+
+while (ValueAbs > 999)
+{
+    if (ValueAbs > 999) ValueAbs /= 10;
+}
+
+Console.WriteLine($"Третья цифра в числе - {ValueAbs % 10}");
