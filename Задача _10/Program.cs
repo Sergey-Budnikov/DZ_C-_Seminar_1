7289de89e9e234ab2cd4e0877004378e6b7d4d82
Будников Сергей Юрьевич <mr.s.b@bk.ru>
2023-02-03 01:24:06 +0300

    Изменение в задаче 10

---
+++ Задача _10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите число");
+int number123 = Convert.ToInt32(Console.ReadLine());
+int number23 = number123 % 100;
+int number2 = number23 / 10;
+Console.WriteLine(number2);
