d5ee858b4c8cd28d7a090c1683020b382a691014
Будников Сергей Юрьевич <mr.s.b@bk.ru>
2023-02-08 14:24:34 +0300

    Решение задачи 27

---
+++ Задача_27/Program.cs
@@ -0,0 +1,35 @@
+﻿// // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+/*Console.WriteLine("Enter Number");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while(number > 1)
+{
+    int temp = number % 10;
+    number = number / 10;
+    sum = sum + temp;
+}
+
+Console.Write(sum);*/
+
+Console.WriteLine("Enter Number");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+
+int Metod(int s, int n)
+{
+    while(n > 1)
+    {
+        int variable = n % 10;
+        n = n / 10;
+        s = s + variable;
+    }
+    return s;
+}
+
+int Summa = Metod(sum, number);
+Console.Write(Summa);
