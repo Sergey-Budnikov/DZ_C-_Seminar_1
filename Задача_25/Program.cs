7c647d735d4732b9eb23887a21094e42d1246e77
Будников Сергей Юрьевич <mr.s.b@bk.ru>
2023-02-08 13:51:07 +0300

    Изменение №1 в задаче 25

--- Задача_25/Program.cs
+++ Задача_25/Program.cs
@@ -21,14 +21,39 @@ int numberA = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter numberB");
 int numberB = int.Parse(Console.ReadLine());
 
-int Metod (int a)
+int Metod (int a, int b)
 {
 int i = 1;
 int temp = a;
-for(; i < numberB; i++)
+for(; i < b; i++)
 {
     a = a * temp;
 }
 return a;
 }
-Console.WriteLine(Metod(numberA));
+Console.WriteLine(Metod(numberA, numberB));
+
+
+
+/*int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}*/
