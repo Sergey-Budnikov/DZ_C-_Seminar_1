39a0c24b609759d59f721841b5aa9ecd9bef7f03
Будников Сергей Юрьевич <mr.s.b@bk.ru>
2023-03-02 14:01:50 +0300

    Решение задачи 53

---
+++ Задача_53/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задайте двухмерный массив. Напишите программу, 
+  которая поменяет местами первую и последнюю строки. */
+
+int [,] matrix = new int[4,4];
+void FillArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+     for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i,j]}");
+        }
+        Console.WriteLine();
+    }
+}
+void Reverse(int[,] array)
+{
+    for( int i = 0; i < array.GetLength(1); i++)
+    {
+        int temp = array[0,i];
+        array[0,i] = array[array.GetLength(0)-1,i];
+        array[array.GetLength(0)-1,i] = temp;
+    }
+}
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+Reverse(matrix);
+PrintArray(matrix);
